4062f6dfef1325e304f404b803f7283c935a1ed0
yerti <yerti0818@gmail.com>
2023-10-01 13:25:51 -0700

    fix pycodestyle

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -15,7 +15,8 @@ def list_get_amenities():
     return jsonify(list_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieves a specific Amenity object by ID"""
 
@@ -26,7 +27,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes a specific Amenity object by ID"""
 
@@ -58,7 +60,8 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates a specific Amenity object by ID"""
 
