c973de23af6bf76959ccfb9634de6ab47f2cf770
yerti <yerti0818@gmail.com>
2023-10-01 13:49:13 -0500

    Fix: file

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,15 +4,17 @@ Creates the variable app that is an instance of Flask
 and handles app.teardown_appcontext
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
-import json
+
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
@@ -22,15 +24,16 @@ def teardown_storage(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    response_data = {"error": "Not found"}
-    response = app.response_class(
-        response=json.dumps(response_data),
-        status=404,
-        mimetype='application/json'
-    )
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
     return response
 
-if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == '__main__':
+    try:
+        h = "0.0.0.0"
+        host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else h
+        port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+        app.run(host=host, port=port, threaded=True)
+    except Exception as e:
+        print(e)
